92eaff4a12302dce3013aabd6eb8f6044148af07
B00M3000 <40374754+B00M3000@users.noreply.github.com>
2023-09-30 15:26:08 -0400

    s

--- server/start.ts
+++ server/start.ts
@@ -1,24 +1,13 @@
 #!/usr/bin/env node
 
-import {promises as fs} from 'fs'
 import * as http from 'http'
-import * as https from 'https'
 import app from './app'
 import {sequelize} from './models'
-import {https as httpsSettings, port} from '../settings.json'
+import {port} from '../settings.json'
 
 sequelize.sync()
 	.then(() => {
-		if (httpsSettings) {
-			const {keyFile, certFile} = httpsSettings
-			return Promise.all([fs.readFile(keyFile), fs.readFile(certFile)])
-				.then(([key, cert]) =>
-					Promise.resolve<http.Server>(https.createServer({key, cert}, app))
-				)
-		}
-		else {
-			return Promise.resolve(http.createServer(app))
-		}
+		return Promise.resolve(http.createServer(app))
 	})
 	.then(server => new Promise<void>((resolve, reject) =>
 		server.listen(port, resolve)
